b5ac999c18384f78241f4c68eb1cde4669442d3c
PauloCoder <williams56@hotmail.fr>
2021-08-12 01:08:39 +0200

    Conception du microservice review et Exploitation du service avec RestTemplate (Microservices)

--- movie/src/main/java/com/mycompany/dvdstore/movie/MovieApplication.java
+++ movie/src/main/java/com/mycompany/dvdstore/movie/MovieApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 
@@ -21,6 +22,12 @@ public class MovieApplication {
 		Hibernate5Module module = new Hibernate5Module();
 		module.disable(Hibernate5Module.Feature.USE_TRANSIENT_ANNOTATION);
 		return module;
+	}
+	
+	@Bean
+	public RestTemplate template()
+	{
+		return new RestTemplate();
 	}
 
 }
