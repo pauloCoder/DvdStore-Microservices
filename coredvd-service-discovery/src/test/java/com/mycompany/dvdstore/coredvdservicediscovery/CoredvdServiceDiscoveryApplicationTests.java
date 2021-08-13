639e8b688c90ff67e4649cbb3730c8b66d505bf6
PauloCoder <williams56@hotmail.fr>
2021-08-14 01:12:53 +0200

    Spring cloud et Client Side Service Discovery avec Netflix Eureka (Microservices)

---
+++ coredvd-service-discovery/src/test/java/com/mycompany/dvdstore/coredvdservicediscovery/CoredvdServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.dvdstore.coredvdservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoredvdServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
